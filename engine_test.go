283638656916ed0d82a8f60a28b23a5b6f248943
Dieter Plaetinck <dieter@grafana.com>
2020-01-12 14:00:34 +0100

    concurrent test

--- engine_test.go
+++ engine_test.go
@@ -2,6 +2,8 @@ package router
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
 	"testing"
 	"time"
 
@@ -49,6 +51,96 @@ func TestAddAndCompare(t *testing.T) {
 				table.Consume(0, uint64(numMetrics), cb)
 				if diff := cmp.Diff(want, got); diff != "" {
 					t.Errorf("testAdd() mismatch (-want +got):\n%s", diff)
+				}
+			})
+		}
+	}
+}
+
+// this version waits before reading, but we should make one that reads at the same time too
+// i mean with a concurrent reader going to null, multiple concurrent readers at various offsets going to null
+// and also validate the results through all these different readers
+func TestAddAndCompareConcurrent(t *testing.T) {
+	insertBatchSizes := []int{2, 3, 7, 31, 137, 5000, 10000, 20000}
+	numMetricss := []int{2, 3, 17, 31, 127, 128, 129, 4094, 4095, 4096, 4097, 4098, 65534, 65535, 65536, 65537, (2 * 65536) - 1, 2 * 65536, (2 * 65536) + 2}
+	//insertBatchSizes = []int{2}
+	//numMetricss = []int{3}
+	for _, numMetrics := range numMetricss {
+		for _, insertBatchSize := range insertBatchSizes {
+			// don't do ridiculous combos
+			// thoough we do want some small-batch concurrency once we deal with more than 1 segment
+			if insertBatchSize < numMetrics/10000 {
+				continue
+			}
+			name := fmt.Sprintf("numMetrics %d insertBatchSize %d", numMetrics, insertBatchSize)
+			t.Run(name, func(t *testing.T) {
+				table := NewTable()
+				metrics := make([]metric, 0, numMetrics)
+				for i := 0; i < numMetrics; i++ {
+					metrics = append(metrics, metric{
+						Key: uint32(i),
+						Ts:  uint32(i),
+					})
+				}
+				var batches [][]metric
+				batchesCopy := make(map[metric][]metric, len(batches))
+				// split up the input in batches
+				var i, j int
+				for j < len(metrics) {
+					j = i + insertBatchSize
+					if j > len(metrics) {
+						j = len(metrics)
+					}
+					batch := metrics[i:j]
+					batchCopy := make([]metric, len(batch))
+					copy(batchCopy, batch)
+					batches = append(batches, batch)
+					batchesCopy[batchCopy[0]] = batchCopy
+					i = j
+				}
+				rand.Shuffle(len(batches), func(i, j int) {
+					batches[i], batches[j] = batches[j], batches[i]
+				})
+				var wg sync.WaitGroup
+				wg.Add(len(batches))
+				for _, batch := range batches {
+					go func(batch []metric) {
+						table.Add(batch)
+						wg.Done()
+					}(batch)
+				}
+				wg.Wait()
+
+				got := make([]metric, 0, numMetrics)
+				cb := func(m metric) {
+					got = append(got, m)
+				}
+				table.Consume(0, uint64(numMetrics), cb)
+
+				// the data will have the batches inserted in random order, though
+				// the batches themselves should be in order.
+				// thus, we need to find all the individual batches
+
+				i = 0
+				for i < len(got) {
+					m := got[i]
+					batch, ok := batchesCopy[m]
+					if !ok {
+						t.Fatalf("did not find batch for metric %v", batch)
+					}
+					batchLen := len(batch)
+					if len(got) < i+batchLen {
+						t.Fatalf("not enough data received to find batch %v", batch)
+					}
+					batchGot := got[i : i+batchLen]
+					if diff := cmp.Diff(batch, batchGot); diff != "" {
+						t.Errorf("testAdd() mismatch (-want +got):\n%s", diff)
+					}
+					delete(batchesCopy, m)
+					i += batchLen
+				}
+				if len(batchesCopy) > 0 {
+					t.Errorf("did not find all batches. %d remaining", len(batchesCopy))
 				}
 			})
 		}
